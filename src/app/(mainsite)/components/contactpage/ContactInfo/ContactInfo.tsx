75d1364457c0f61d758eb420d694f75eec737674
William Bojczuk <williambojczuk@gmail.com>
2024-06-24 06:39:09 -0400

    updated

--- src/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo.tsx
+++ src/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo.tsx
@@ -34,7 +34,7 @@ export default function ContactInfo() {
           <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M32 376a56 56 0 0 0 56 56h336a56 56 0 0 0 56-56V222H32Zm66-76a30 30 0 0 1 30-30h48a30 30 0 0 1 30 30v20a30 30 0 0 1-30 30h-48a30 30 0 0 1-30-30ZM424 80H88a56 56 0 0 0-56 56v26h448v-26a56 56 0 0 0-56-56"></path></svg>
             <span>We accept all major credit cards and Cash App.
 <br /><br />
-*Booking requires a 50% non-refundable deposit. The remaining balance is payable upon completion of service.</span>
+*Booking requires a non-refundable deposit. The remaining balance is payable upon completion of service.</span>
           </a>
 
         </div>
--- src/app/(mainsite)/components/misc/Footer/Footer.tsx
+++ src/app/(mainsite)/components/misc/Footer/Footer.tsx
@@ -70,7 +70,7 @@ export default function Footer() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M32 376a56 56 0 0 0 56 56h336a56 56 0 0 0 56-56V222H32Zm66-76a30 30 0 0 1 30-30h48a30 30 0 0 1 30 30v20a30 30 0 0 1-30 30h-48a30 30 0 0 1-30-30ZM424 80H88a56 56 0 0 0-56 56v26h448v-26a56 56 0 0 0-56-56"></path></svg>
                   <a href="https://g.co/kgs/bj9aMXS" target="_blank">We accept all major credit cards and Cash App.
 <br /><br />
-*Booking requires a 50% non-refundable deposit. The remaining balance is payable upon completion of service.</a>
+*Booking requires a non-refundable deposit. The remaining balance is payable upon completion of service.</a>
                 </aside>
 
               </ul>
