dcfdff07b4253b1c9aa2c59397fbd074ed5d1ecc
William Bojczuk <williambojczuk@gmail.com>
2024-07-08 19:52:40 -0400

    updated

--- src/app/(mainsite)/components/homepage/BounceHouse/BounceHouse.tsx
+++ src/app/(mainsite)/components/homepage/BounceHouse/BounceHouse.tsx
@@ -10,7 +10,7 @@ return (
     <h2>Need A Bounce House<span className="highlight">?</span></h2>
     <p>We’ve got you covered! Ask us about getting a bounce house to make your party an unforgettable experience!</p>
     <div className={`center ${styles.linkWrapper}`}>
-    <Link className='main-link' href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUM}`}>Call / Text</Link>
+    <Link className='main-link' href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUM}`}>Call Now</Link>
     </div>
     <div className="shader-dark-2" ></div>
     <img src='/img/bouncehouse.png' alt='Image of people partying' className='bg-img' />
