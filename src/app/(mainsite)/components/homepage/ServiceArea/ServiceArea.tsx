9ca4494345cd47714e83526f046b29212a129952
William Bojczuk <williambojczuk@gmail.com>
2026-03-02 19:11:17 -0500

    updated

--- src/app/(mainsite)/components/homepage/ServiceArea/ServiceArea.tsx
+++ src/app/(mainsite)/components/homepage/ServiceArea/ServiceArea.tsx
@@ -6,7 +6,7 @@ return (
  <div className={styles.serviceArea}>
     
     <div className={styles.mapWrapper}>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4925.434663625484!2d-81.0509694!3d41.75962339999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883190e7cdf958df%3A0xbf4ac543ddb85a8b!2s201%20Water%20Tower%20Dr%2C%20Madison%2C%20OH%2044057!5e1!3m2!1sen!2sus!4v1745063407271!5m2!1sen!2sus" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d79019.07460061104!2d-78.9552530459185!3d43.24201665762391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d362139d69b711%3A0x2d6a92246ed471b9!2sRansomville%2C%20NY%2014131!5e1!3m2!1sen!2sus!4v1772496447209!5m2!1sen!2sus" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
  </div>
 )};
